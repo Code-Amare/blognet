29a8d21b43059a294c89991923024099d11a9ecb
Code-Amare <amisgana809@gmail.com>
2026-08-04 04:07:16 +0300

    feat: set vercel to allowed host

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-^pufqq4ouq)927*rd79+yr0y+xq@44xu1=afaa-j@0pzkh+(7p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app",]
 
 
 # Application definition
